a9fb55519278c08585510655ff317fcaef9b322f
xdevl <xdevl@users.noreply.github.com>
2019-12-05 17:42:36 +0000

    Added the ability for select component options to be loaded dynamically

--- lib/select.tsx
+++ lib/select.tsx
@@ -3,63 +3,73 @@
  * This work is licensed under the terms of the MIT license.
  * For a copy, see <https://opensource.org/licenses/MIT>.
  */
+import {StandardProps} from "@material-ui/core";
 import Checkbox from "@material-ui/core/Checkbox";
 import Chip from "@material-ui/core/Chip";
 import ListItemText from "@material-ui/core/ListItemText";
 import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
-import React from "react";
+import Select, {SelectClassKey, SelectProps} from "@material-ui/core/Select";
+import React, {useState} from "react";
 
 const defaultEmptyMessage = "Select...";
 
-interface ISingleSelect<T> {
-    options: T[];
-    value: T;
+type Options<T> = T[] | (() => Promise<T[]>);
+
+const promisify = <T, >(options: Options<T>) => options instanceof Function ? options() : Promise.resolve(options);
+
+interface ISelect<T, V> extends StandardProps<SelectProps, SelectClassKey, "value"|"onChange"|"onOpen"> {
+    options: Options<T>;
+    value?: V;
     getKey: (item: T) => string;
     getLabel: (item: T) => string;
-    onChange: (item: T) => void;
-    fullWidth?: boolean;
+    onChange: (item: V) => void;
     emptyMessage?: string;
 }
 
-export const SingleSelect = <T, >(props: ISingleSelect<T>) => {
-    const map = new Map(props.options.map((option) => [props.getKey(option), option]));
+export const SingleSelect = <T, >(properties: ISelect<T, T>) => {
 
-    return <Select value={props.getKey(props.value)} displayEmpty fullWidth={props.fullWidth}
-            onChange={(event) => props.onChange(map.get(event.target.value as string)!)}>
-        <MenuItem value={undefined} disabled>{props.emptyMessage || defaultEmptyMessage}</MenuItem>
-        {props.options.map((option) => (
-            <MenuItem key={props.getKey(option)} value={props.getKey(option)}>
-                {props.getLabel(option)}
-            </MenuItem>
+    const {options, value, getKey, getLabel, onChange, emptyMessage, ...selectProps} = properties;
+    const [optionValues, setOptionValues] = useState(options instanceof Array ? options : []);
+    const loadOptionValues = () => {
+        promisify(options).then((result) => setOptionValues(result));
+    };
+
+    const map = new Map(optionValues.concat(value ? [value] : [])
+        .map((option) => [getKey(option), option]));
+
+    return <Select {...selectProps} value={(value && getKey(value)) || ""}
+            onOpen={loadOptionValues}
+            onChange={(event) => onChange(map.get(event.target.value as string)!)}>
+        <MenuItem value="" disabled>{emptyMessage || defaultEmptyMessage}</MenuItem>
+        {Array.from(map.entries()).map(([key, option]) => (
+            <MenuItem key={key} value={key}>{getLabel(option)}</MenuItem>
         ))}
     </Select>;
 };
 
-interface IMultipleSelect<T> {
-    options: T[];
-    value: T[];
-    getKey: (item: T) => string;
-    getLabel: (item: T) => string;
-    onChange: (item: T[]) => void;
-    fullWidth?: boolean;
-    emptyMessage?: string;
-}
+export const MultipleSelect = <T, >(properties: ISelect<T, T[]>) => {
 
-export const MultipleSelect = <T, >(props: IMultipleSelect<T>) => {
-    const map = new Map(props.options.map((option) => [props.getKey(option), option]));
-    const keys = props.value.map((option) => props.getKey(option));
+    const {options, value, getKey, getLabel, onChange, emptyMessage, ...selectProps} = properties;
+    const [optionValues, setOptionValues] = useState(options instanceof Array ? options : []);
+    const loadOptions = () => {
+        promisify(options).then((result) => setOptionValues(result));
+    };
 
-    return <Select value={keys} multiple displayEmpty fullWidth={props.fullWidth}
-            onChange={(event) => props.onChange((event.target.value as string[]).map((value) => map.get(value)!))}
+    const map = new Map(optionValues.concat(value || []).map((option) => [getKey(option), option]));
+    const selection = (value || []).map((option) => getKey(option));
+
+    return <Select {...selectProps} value={selection} multiple
+            onChange={(event) => onChange((event.target.value as string[]).map((key) => map.get(key)!))}
+            onOpen={loadOptions}
             renderValue={(selected) => (selected as string[]).length > 0 ? <React.Fragment>
-                {(selected as string[]).map((value) =>
-                    <Chip key={value} label={props.getLabel(map.get(value)!)} style={{marginRight: "1em"}} />)}
-            </React.Fragment> : (props.emptyMessage || defaultEmptyMessage)}>
-        {props.options.map((option) => (
-            <MenuItem key={props.getKey(option)} value={props.getKey(option)}>
-                <Checkbox checked={keys.indexOf(props.getKey(option)) >= 0} />
-                <ListItemText primary={props.getLabel(option)} />
+                {(selected as string[]).map((key) =>
+                    <Chip key={key} label={getLabel(map.get(key)!)} style={{marginRight: "1em"}} />)}
+            </React.Fragment> : selectProps.displayEmpty && (emptyMessage || defaultEmptyMessage)}>
+        <MenuItem value="" disabled>{emptyMessage || defaultEmptyMessage}</MenuItem>
+        {Array.from(map.entries()).map(([key, option]) => (
+            <MenuItem key={key} value={key}>
+                <Checkbox checked={selection.indexOf(key) >= 0} />
+                <ListItemText primary={getLabel(option)} />
             </MenuItem>
         ))}
     </Select>;
